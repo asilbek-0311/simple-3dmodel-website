50b4811ab33e0c2d517bd8a50a17d630d22cd0d5
Asilbek-0311 <azizjonogliasilbek@gmail.com>
2024-07-13 11:58:09 +0800

    fized the bug, it is rendering now, yeay

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.136.0';
-import { FBXLoader } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/FBXLoader.js'
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-//create scene
+const canvas = document.getElementById('canvas');
 const scene = new THREE.Scene();
-
-//create camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 2, 5);
 
-//create a rendere adn set its size
-const renderer = new THREE.WebGLRenderer({canvas: document.getElementById('canvas')});
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+// canvas.appendChild(renderer.domElement);
 
-//add a light to the scene
-const directionLight = new THREE.DirectionalLight(0xffffff, 100);
-directionLight.position.set(1, 1, 1);
-scene.add(directionLight);
+//mkaing the 3d object global so i can access later
+let object, controls;
+let objToRender = './model/scene.gltf';
 
-const ambientLight = new THREE.AmbientLight()
-scene.add(ambientLight);
+console.log(objToRender);
 
-
-//load the fbx model
-const loader = new FBXLoader();
-loader.load('./Floating.fbx', 
-    function (object) {
+const loader = new GLTFLoader();
+loader.load(objToRender, 
+    function(gltf) {
+        object = gltf.scene;
         scene.add(object);
     },
-    undefined,
-    function (error) {
+    function(xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function(error) {
         console.error(error);
-    }
-);
+    });
 
-//animation loop
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+
+camera.position.z = 15;
+camera.position.y = 4;
+// camera.position.x = 3;
+
+const directLight = new THREE.DirectionalLight(0xffffff, 10);
+directLight.position.set(20, 20, 20);
+scene.add(directLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 3);
+scene.add(ambientLight);
+
+controls = new OrbitControls(camera, renderer.domElement);
+
 function animate() {
     requestAnimationFrame(animate);
+
     renderer.render(scene, camera);
 }
 
-animate();
+window.addEventListener("resize", function() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
